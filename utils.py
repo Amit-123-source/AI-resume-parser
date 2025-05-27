bbb08a9f4bf540a6e8953556c2ddfa99c3899420
Amit Kumar Swain <73372897+Amit-123-source@users.noreply.github.com>
2025-05-27 20:03:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 def save_to_csv(data, output_file='extracted_resume_data.csv'):
-    """ Saves the extracted data into a CSV file. """
     df = pd.DataFrame(data)
     df.to_csv(output_file, index=False)
+    print(f"Saved CSV to {output_file}")
 
 def save_to_excel(data, output_file='extracted_resume_data.xlsx'):
-    """ Saves the extracted data into an Excel file. """
     df = pd.DataFrame(data)
     df.to_excel(output_file, index=False, engine='openpyxl')
+    print(f"Saved Excel to {output_file}")
